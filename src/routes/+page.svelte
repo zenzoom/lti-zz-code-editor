2f0fd3d7489611eb7e4e20d69d4653956613b706
John Koss <kossjohn@gmail.com>
2024-09-03 17:53:36 -0400

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import CodeMirror from "svelte-codemirror-editor";
+    import { javascript } from "@codemirror/lang-javascript";
+
+    let value = "";
+</script>
+
+<CodeMirror bind:value lang={javascript()} />
